b9b10f40fb28cd9050750086178ea4944e32632c
Brian-Rojas-B <72364530+Brian-Rojas-B@users.noreply.github.com>
2021-06-23 00:13:59 -0500

    Update MapFragment.java

--- app/src/main/java/com/example/compraagro/fragments/MapFragment.java
+++ app/src/main/java/com/example/compraagro/fragments/MapFragment.java
@@ -3,14 +3,18 @@ package com.example.compraagro.fragments;
 import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.compraagro.MainActivity;
 import com.example.compraagro.R;
+import com.example.compraagro.model.Product;
+import com.example.compraagro.model.User;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -19,6 +23,11 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.TileOverlay;
 import com.google.android.gms.maps.model.TileOverlayOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.maps.android.heatmaps.HeatmapTileProvider;
 
 import java.util.ArrayList;
@@ -33,6 +42,8 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         View root = inflater.inflate(R.layout.fragment_map, container, false);
 
 
+        //Internet
+        //Cargar el mapa
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -45,7 +56,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
 
 
 
-
+    //Cuando el mapa este listo
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -55,30 +66,59 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
 //        mMap.addMarker(new MarkerOptions().position(sydney).title("Tacna"));
 //        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
+        //Habilitar zoom
         mMap.getUiSettings().setZoomControlsEnabled(true);
 
+        //Caargar mapa de calor
         loadHeatMap();
 
     }
 
     private void loadHeatMap(){
 
-        HeatmapTileProvider mProvider;
-        TileOverlay mOverlay;
+        //Centrar la camara
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(-16.39889, -71.535),7));
-        ArrayList<LatLng> list = new ArrayList<>();
-        list.add(new LatLng(-16.39889, -71.535));
-        list.add(new LatLng(-13.52264, -71.96734));
-        list.add(new LatLng(-17.19832, -70.93567));
-        list.add(new LatLng(-12.04318, -77.02824));
-        list.add(new LatLng(-18.01465, -70.25362));
-        list.add(new LatLng(-18.01465, -70.25362));
-        list.add(new LatLng(-18.01465, -70.25362));
+        //Agregar puntos
 
-        mProvider = new HeatmapTileProvider.Builder()
-                .data(list)
-                .build();
-        mOverlay = mMap.addTileOverlay(new TileOverlayOptions().tileProvider(mProvider));
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Products");
+
+        ArrayList<LatLng> list = new ArrayList<>();
+
+        reference.addValueEventListener(new ValueEventListener() {
+
+
+            HeatmapTileProvider mProvider;
+            TileOverlay mOverlay;
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    Product product = snapshot.getValue(Product.class);
+                    if(product.getLat()!=null){
+
+                        LatLng position = new LatLng(Double.parseDouble(product.getLat()), Double.parseDouble(product.getLng()));
+                        list.add(position);
+
+                        mMap.addMarker(new MarkerOptions().position(position).title(product.getNombre()));
+
+                    }
+                }
+
+
+                //Contruir el mapa de calor
+                mProvider = new HeatmapTileProvider.Builder()
+                        .data(list)
+                        .build();
+                mOverlay = mMap.addTileOverlay(new TileOverlayOptions().tileProvider(mProvider));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
 
     }
 
