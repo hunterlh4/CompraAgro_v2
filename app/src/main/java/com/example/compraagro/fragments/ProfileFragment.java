0887ca142f03b9c62f06966b836c8f749fb8aaa3
Brian-Rojas-B <72364530+Brian-Rojas-B@users.noreply.github.com>
2021-06-23 00:10:29 -0500

    Mis cambios

--- app/src/main/java/com/example/compraagro/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/compraagro/fragments/ProfileFragment.java
@@ -1,5 +1,9 @@
 package com.example.compraagro.fragments;
 
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -8,12 +12,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.MimeTypeMap;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.compraagro.R;
 import com.example.compraagro.model.User;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -21,9 +31,22 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.HashMap;
+
+import static android.app.Activity.RESULT_OK;
 
 public class ProfileFragment extends Fragment {
+
+
+
+    private static final int IMAGE_REQUEST = 1;
+    private Uri imageUri;
+    private StorageTask uploadTask;
 
     TextView tvNombres,tvApellidos,tvEmail,tvDepartamento,tvTelefono;
     ImageView profileImage;
@@ -48,8 +71,14 @@ public class ProfileFragment extends Fragment {
 
         fuser = FirebaseAuth.getInstance().getCurrentUser();
         reference = FirebaseDatabase.getInstance().getReference("Users").child(fuser.getUid());
+        storageReference = FirebaseStorage.getInstance().getReference("uploads");
 
-
+        profileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
+        });
 
         reference.addValueEventListener(new ValueEventListener() {
             @Override
@@ -62,6 +91,7 @@ public class ProfileFragment extends Fragment {
                 tvEmail.setText(user.getEmail());
                 tvDepartamento.setText(user.getDepartamento());
                 tvTelefono.setText(user.getTelefono());
+                Glide.with(getContext()).load(user.getUrlImagen()).into(profileImage);
 
             }
 
@@ -74,5 +104,110 @@ public class ProfileFragment extends Fragment {
 
 
         return root;
+    }
+
+
+
+    private void openGallery(){
+
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, IMAGE_REQUEST);
+    }
+
+    private String getFileExtension(Uri uri){
+        ContentResolver contentResolver = getContext().getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
+    }
+    private void uploadImage(){
+        final ProgressDialog pd = new ProgressDialog(getContext());
+        pd.setMessage("Subiendo");
+        pd.show();
+
+        if (imageUri != null){
+            final  StorageReference fileReference = storageReference.child(System.currentTimeMillis()
+                    +"."+getFileExtension(imageUri));
+
+            uploadTask = fileReference.putFile(imageUri);
+            uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                @Override
+                public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                    if (!task.isSuccessful()){
+                        throw  task.getException();
+                    }
+
+                    return  fileReference.getDownloadUrl();
+                }
+            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task) {
+                    if (task.isSuccessful()){
+                        Uri downloadUri = task.getResult();
+                        String mUri = downloadUri.toString();
+
+                        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users");
+
+
+                        reference.addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                                    User user = snapshot.getValue(User.class);
+                                    if(user.getId().equals(fuser.getUid())){
+                                        User updateUser= new User();
+                                        updateUser.setId(user.getId());
+                                        updateUser.setNombres(user.getNombres());
+                                        updateUser.setApellidos(user.getApellidos());
+                                        updateUser.setTelefono(user.getTelefono());
+                                        updateUser.setDepartamento(user.getDepartamento());
+                                        updateUser.setTipoUsuario("Vendedor");
+                                        updateUser.setEmail(user.getEmail());
+                                        updateUser.setUrlImagen(mUri);
+                                        reference.child(fuser.getUid()).setValue(updateUser);
+                                    }
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+
+                                Toast.makeText(getActivity(),"Cancelado",Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+                        pd.dismiss();
+                    } else {
+                        Toast.makeText(getContext(), "Fallo!", Toast.LENGTH_SHORT).show();
+                        pd.dismiss();
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                    pd.dismiss();
+                }
+            });
+        } else {
+            Toast.makeText(getContext(), "Ninguna imagen seleccionada", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null){
+            imageUri = data.getData();
+
+            if (uploadTask != null && uploadTask.isInProgress()){
+                Toast.makeText(getContext(), "Subiendo", Toast.LENGTH_SHORT).show();
+            } else {
+                uploadImage();
+            }
+        }
     }
 }
